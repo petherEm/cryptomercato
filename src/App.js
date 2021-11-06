ba4ffbe594baa0fc249bcc85ce14a5fc63b11959
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2021-11-06 17:23:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Link, Routes, Switch } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
+import { Exchanges } from './components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="app">
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route exact path="/" element={<Homepage />}>
+                                
+                            </Route>
+                            <Route path="/exchanges" element={<Exchanges />}>
+                                
+                            </Route>
+                            <Route path="/cryptocurrencies" element={<Cryptocurrencies />}>
+                                
+                            </Route>
+                            <Route path="/crypto/:coinId" element={<CryptoDetails />}>
+                                
+                            </Route>
+                            <Route path="/news" element={<News />}>
+                             
+                            </Route>
+                        </Routes>
+                    </div>
+                </Layout>
+                
+            
+                <div className="footer"  >
+                    <Typography.Title level={5} style={{ color: "white", textAlign: "center"}}>
+                        Adapted by <a href="https://piotrmaciejewski.com">Piotr Maciejewski</a><br />
+                        from <a href="https://www.completepathtojavascriptmastery.com" target="_blank">JavaScriptMastery</a>
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,53 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'd92cf13a56mshbff7f35025b50bfp1e2f27jsn9fcb7acd2b10'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`)
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`)
+        }),
+    })
+})
+
+
+export const {
+    useGetCryptosQuery,
+    useGetCryptoDetailsQuery,
+    useGetCryptoHistoryQuery,
+    useGetExchangesQuery
+} = cryptoApi;
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/stats',
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': 'd92cf13a56mshbff7f35025b50bfp1e2f27jsn9fcb7acd2b10'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
